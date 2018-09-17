79e8b06e60a697f1cd44c886bc0674639a5ffe90
Egor <ea-dorosh@yandex.ru>
2018-09-17 17:00:58 +0400

    последние доработки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+// переменные для "напишите нам" //
+
 var writeUsLink = document.querySelector(".btn-write-us");
 var popupWriteUs = document.querySelector(".modal-write-us");
 var close = document.querySelector(".modal-close");
@@ -5,14 +8,23 @@ var form = popupWriteUs.querySelector(".write-us-form");
 var yourName = popupWriteUs.querySelector("[name=name]");
 var yourEMail = popupWriteUs.querySelector("[name=e-mail]");
 var yourMessage = popupWriteUs.querySelector("[name=message]");
+var isStorageSupport = true;
+var storageName = "";
+var storageEMail = "";
+
+// переменные для "найти нас на карте" //
 
 var mapLink = document.querySelector(".btn-popup-map");
 var popupMap = document.querySelector(".modal-map");
 var closeMap = popupMap.querySelector(".modal-close");
 
-var isStorageSupport = true;
-var storageName = "";
-var storageEMail = "";
+// переменные для "корзины" //
+
+var buyButtons = document.querySelectorAll(".btn-buy");
+var popupCart = document.querySelector(".modal-cart");
+var closeCart = popupCart.querySelector(".modal-close");
+
+// код для "напишите нам" //
 
 try {
 	storageName = localStorage.getItem("yourName");
@@ -24,17 +36,18 @@ try {
 writeUsLink.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popupWriteUs.classList.add("modal-show");
-	if (storageName) {
+	if (storageName && storageEMail) {
 		yourName.value = storageName;
-		yourEMail.focus();
-	} else {
-		yourName.focus();
-	}
-	if (storageEMail) {
 		yourEMail.value = storageEMail;
 		yourMessage.focus();
-	} else {
+	} else if (storageName) {
+		yourName.value = storageName;
 		yourEMail.focus();
+	} else if (storageEMail) {
+		yourEMail.value = storageEMail;
+		yourName.focus();
+	} else {
+		yourName.focus();
 	}
 });
 
@@ -68,6 +81,8 @@ window.addEventListener("keydown", function(evt) {
 	}
 });
 
+// код для "найти нас на карте" //
+
 mapLink.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popupMap.classList.add("modal-show");
@@ -86,3 +101,27 @@ window.addEventListener("keydown", function(evt) {
 		}
 	}
 });
+
+// код для "корзины" //
+
+for (var i = 0; i <buyButtons.length; i++ ) {
+	buyButtons[i].addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popupCart.classList.add("modal-show");
+	});
+};
+
+closeCart.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupCart.classList.remove("modal-show")
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (popupCart.classList.contains("modal-show")) {
+		evt.preventDefault();
+		popupCart.classList.remove("modal-show");
+	}
+});
+
+
+
